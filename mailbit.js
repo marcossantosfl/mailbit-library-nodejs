c4d529174aee6d81cbd6e359a3dd2ad35f06449d
marcossantosfl <56878202+marcossantosfl@users.noreply.github.com>
2024-07-12 06:36:22 +0700

    update http instance.

--- mailbit.js
+++ mailbit.js
@@ -3,10 +3,21 @@ const axios = require('axios');
 class Mailbit {
     constructor(apiKey, baseUrl = 'https://public-api.mailbit.io') {
         if (!apiKey) {
-            throw new Error("API key is required"); 
+            throw new Error("API key is required");
         }
         this.apiKey = apiKey;
         this.baseUrl = baseUrl;
+
+        // Create an axios instance with default configuration
+        this.client = axios.create({
+            baseURL: this.baseUrl,
+            headers: {
+                'token': this.apiKey,
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/json'
+            },
+            timeout: 30000,
+        });
     }
 
     static generateErrorMessage(code, message) {
@@ -14,15 +25,26 @@ class Mailbit {
     }
 
     async sendEmail(emailData) {
-        const url = `${this.baseUrl}/send-email`;
+        const url = '/send-email';
 
         try {
-            const response = await axios.post(url, emailData, { headers: { 'token': this.apiKey } });
+            const response = await this.client.post(url, emailData);
             console.log('Email sent successfully:', response.data.message);
             return response.data;
         } catch (err) {
             if (err.response) {
-                const { code, message } = err.response.data;
+                const responseBody = err.response.data;
+                let message;
+                let code;
+
+                if (Array.isArray(responseBody.errors)) {
+                    message = responseBody.errors.map(error => `Code: ${error.code}, Message: ${error.message}`).join(' | ');
+                    code = err.response.status;
+                } else {
+                    code = responseBody.code || 'Unknown';
+                    message = responseBody.message || 'No error message provided';
+                }
+
                 console.error(`Error sending email\nCode: ${code}\nMessage: ${message}`);
                 throw Mailbit.generateErrorMessage(code, message);
             } else if (err.request) {
